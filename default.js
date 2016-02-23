a8dc110ae1c54f984cafb37801d998ab1297e428
aaronmarkle <aaronkmarkle@gmail.com>
2016-02-22 23:18:17 -0800

    modal populates a dropdown list of events from api call #3

--- default.js
+++ default.js
@@ -1,7 +1,6 @@
 app = angular.module('ticketSocket', ['ui.bootstrap', 'ngAnimate']);
 
 app.controller('buyTicketsModal', function($scope, $uibModal, $log) {
-  $scope.items = ['option 1', 'option 2', 'option 3'];
   $scope.animationEnabled = true;
   $scope.open = function() {
     var modalInstance = $uibModal.open({
@@ -23,15 +22,23 @@ app.controller('buyTicketsModal', function($scope, $uibModal, $log) {
   };
 });
 
-app.controller('ModalInstanceCtrl', function($scope, $uibModalInstance, items) {
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
+app.controller('ModalInstanceCtrl', function($scope, $uibModalInstance, getEvents) {
+  getEvents.getEvents().then(function(response) {
+    $scope.eventSelection = response.data;
+  });
   $scope.ok = function() {
     $uibModalInstance.close($scope.selected.item);
   };
   $scope.cancel = function() {
     $uibModalInstance.dismiss('cancel');
   };
+});
+
+app.factory('getEvents', function($http) {
+  function getEvents() {
+    return $http.get('http://rcastillo.ticketsocket.com/api/v1/events');
+  }
+  return {
+    getEvents: getEvents
+  }
 });
